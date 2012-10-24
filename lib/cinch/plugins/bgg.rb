5e4b2e2f7738ad04e396a8861457bc3d5da355a5
Caitlin Woodward <cwoodward@coroutine.com>
2012-10-23 22:18:49 -0500

    Update to bgg with bgg api gem

--- lib/cinch/plugins/bgg.rb
+++ lib/cinch/plugins/bgg.rb
@@ -1,31 +1,79 @@
 require 'cinch'
 require 'open-uri'
 require 'nokogiri'
+require File.expand_path(File.dirname(__FILE__)) + '/lib/bgg_api'
 
 module Cinch
   module Plugins
     class Bgg
       include Cinch::Plugin
 
-      match /bgg (.+)/i,     method: :bgg
-      match /bgguser (.+)/i, method: :bgguser
+      USERS_FILE = "data/users"
+
+      match /bgg (.+)/i,        method: :bgg
+      match /bggme/i,           method: :bggself
+      match /bgguser (.+)/i,    method: :bgguser
+      match /bgguser -u (.+)/i, method: :bggactualuser
+
+      match /link_bgg (.+)/i,   method: :link_to_bgg
+
+      match /whohas (.+)/i,     method: :who_has_game
+
+      def initialize(*args)
+        super
+
+        @bgg = BggApi.new
+        @community = load_community
+      end
 
       def bgg(m, title)
-        results = search_bgg(title)
-        if results == "not found"
-          m.reply "#{m.user.nick}: \"#{title}\" not found"
-        elsif results == "too big"
-          m.reply "#{m.user.nick}: \"#{title}\" was too broad of a search term"
-        else
-          game = results.sort{ |x, y| x.rank <=> y.rank }.first
-          m.reply "#{m.user.nick}: #{game.name} (#{game.year}) - #{game.rating} - Rank: #{game.game_rank} - Designer: #{game.designers.join(", ")} - Mechanics: #{game.mechanics.join(", ")} - " <<
-            "http://boardgamegeek.com/boardgame/#{game.id}"
+        game = search_bgg(m, title)
+        unless game.nil?
+          m.reply "#{game.name} (#{game.year}) - #{game.rating} - Rank: #{game.game_rank} - Designer: #{game.designers.join(", ")} - Mechanics: #{game.mechanics.join(", ")} - " <<
+            "http://boardgamegeek.com/boardgame/#{game.id}", true
         end
       end
 
-      def bgguser(m, name)
-        user = search_for_user(name)
-        m.reply "#{m.user.nick}: #{user.name} - Collection: #{user.collection.size} - Top 5: #{user.top_games.first(5).join(", ")} - http://boardgamegeek.com/user/#{user.name}"
+      def bgguser(m, nick)
+        unless nick.start_with?("-u ")
+          if self.in_community?(nick)
+            name = self.find_bgg_by_irc(nick)
+            user = search_for_user(name)
+            m.reply "#{user.name} - Collection: #{user.collection.size} - Top 5: #{user.top_games.first(5).join(", ")} - http://boardgamegeek.com/user/#{user.name}", true
+          else
+            m.reply "There's no \"#{nick}\" in the community. To link yourself and join the community, \"!link_bgg bgg_username\". To search by actual bgg username, \"!bgguser -u bgg_username\".", true
+          end
+        end
+      end
+
+      def bggself(m)
+        self.bgguser(m.user.nick)
+      end
+
+      def bggactualuser(m, nick)
+        user = search_for_user(nick)
+        m.reply "#{user.name} - Collection: #{user.collection.size} - Top 5: #{user.top_games.first(5).join(", ")} - http://boardgamegeek.com/user/#{user.name}", true        
+      end
+
+      def link_to_bgg(m, username)
+        @community[m.user.nick] = username
+        File.open(USERS_FILE, 'w') do |file|
+          @community.each do |irc_nick, bgg_name|
+            file.write("#{irc_nick},#{bgg_name}\n")
+          end
+        end
+        m.reply "You are now added to the community!", true
+      end
+
+      def who_has_game(m, title)
+        game = search_bgg(m, title)
+        unless game.nil?
+          community = @community.dup
+          community.each{ |irc, bgg| community[irc] = search_for_user(bgg, { :id => game.id }) }
+          
+          community.keep_if{ |irc, user| user.collection.include? game.id.to_s }
+            m.reply "Owning \"#{game.name}\": #{community.keys.join(", ")}", true
+        end
       end
 
       #--------------------------------------------------------------------------------
@@ -33,22 +81,23 @@ module Cinch
       #--------------------------------------------------------------------------------
       protected
 
-      def search_bgg(search_string)
-        search_results_xml = Nokogiri::XML(open("http://boardgamegeek.com/xmlapi2/search?query=#{search_string.gsub(" ", "%20")}&type=boardgame&exact=1").read)
-        if search_results_xml.css('items')[0]['total'] == "0"
-          search_results_xml = Nokogiri::XML(open("http://boardgamegeek.com/xmlapi2/search?query=#{search_string.gsub(" ", "%20")}&type=boardgame").read)
+      def search_bgg(m, search_string)
+        search_results = @bgg.search({:query => search_string, :type => 'boardgame', :exact => 1})
+        if search_results['total'] == "0"
+          search_results = @bgg.search({:query => search_string, :type => 'boardgame'})
         end
-        search_results = search_results_xml.css('item').map { |i| i['id'].to_i }
+        search_results = search_results["item"].map { |i| i['id'].to_i }
         
         # this is dumb, find a better way
         if search_results.empty?
-          response = "not found"
+          m.reply "\"#{title}\" not found", true
         elsif search_results.size > 50
-          response = "too big"
+          m.reply "\"#{title}\" was too broad of a search term", true
         else
-          response = search_results.map do |id|
+          results = search_results.map do |id|
             Game.new(id, get_info_for_game(id))
           end
+          response = results.sort{ |x, y| x.rank <=> y.rank }.first
         end
         response
       end
@@ -64,10 +113,36 @@ module Cinch
         Nokogiri::XML(File.open("data/games/#{game_id}.xml"))
       end
 
-      def search_for_user(name)
-        user_xml = Nokogiri::XML(open("http://boardgamegeek.com/xmlapi2/user?name=#{name}&hot=1&top=1").read)
-        collection_xml = Nokogiri::XML(open("http://boardgamegeek.com/xmlapi2/collection?username=#{name}&own=1").read)
-        User.new(name, user_xml, collection_xml)
+      def search_for_user(name, collection_options = {})
+        user = @bgg.user({:name => name, :hot => 1, :top => 1})      
+        collection = @bgg.collection({:username => name, :own => 1, :stats => 1}.merge(collection_options))   
+        puts "="*80
+        puts collection.inspect
+        puts "="*80
+                  
+        user = User.new(name, user, collection)
+        puts "="*80
+        puts user.inspect
+        puts "="*80
+        user
+      end
+
+      def load_community
+        users_file = File.open(USERS_FILE)
+        users = {}
+        users_file.lines.each do |line|
+          nicks = line.gsub("\n", "").split(",")
+          users[nicks.first] = nicks.last
+        end        
+        users
+      end
+
+      def find_bgg_by_irc(irc_nick)
+        @community[irc_nick]
+      end
+
+      def in_community?(irc_nick)
+        self.find_bgg_by_irc(irc_nick) != nil
       end
 
     end
@@ -107,11 +182,18 @@ module Cinch
 
       def initialize(username, user_xml, collection_xml)
         self.name       = username
-        self.top_games  = user_xml.css("top item").map{ |g| g["name"] }
-        self.hot_games  = user_xml.css("hot item").map{ |g| g["name"] }
+        self.top_games  = user_xml["top"].nil? ? nil : user_xml["top"].select{|h| h["domain"] == "boardgame" }.first["item"].map{ |g| g["name"] }
+        self.hot_games  = user_xml["hot"].nil? ? nil : user_xml["hot"].select{|h| h["domain"] == "boardgame" }.first["item"].map{ |g| g["name"] }
         self.collection = {}
-        collection_xml.css("items item").each do |g| 
-          self.collection[g["objectid"]] = g.css("name")[0].content
+        unless collection_xml["item"].nil?
+          collection_xml["item"].each do |g| 
+            self.collection[g["objectid"]] = {}
+            self.collection[g["objectid"]]["name"]      = g["name"].first["content"] 
+            self.collection[g["objectid"]]["for_trade"] = g["status"].first["fortrade"] 
+            self.collection[g["objectid"]]["want"]      = g["status"].first["want"] 
+            self.collection[g["objectid"]]["plays"]     = g["numplays"].first
+            self.collection[g["objectid"]]["ratings"]   = g["stats"].first["rating"].first["value"]
+          end
         end
       end
 
